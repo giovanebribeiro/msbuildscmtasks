74b03e12824932f8891a3149183c88a639fa0519
Giovane <giovanebribeiro@gmail.com>
2015-11-17 17:12:10 -0300

    Add project structure

---
+++ GeneralAssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MSBuild SCM Tasks")]
+[assembly: AssemblyDescription("A set of MSBuild tasks to make SCM procedures")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: CLSCompliant(true)]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("-1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")] // not used 
